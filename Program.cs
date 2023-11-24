32341e71eb814c59512bd1429fb05d9a7b206bca
FowziPirat12 <fowzino16@gmail.com>
2023-11-24 10:32:00 +0100

    klar

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Program
+{
+   static void Main()
+    {
+      
+      string ord = Console.ReadLine();
+      int antal = int.Parse(Console.ReadLine());
+      
+      if (ord.Length < 9 || antal < 9)
+      {
+         Console.WriteLine("Får ej överstiva 9");
+      }
+      
+      else
+      {
+         for (int i = 0; i < antal; i++)
+         {
+            Console.WriteLine(ord + " ");
+         }
+      }
+    }
+}
+
